65a266530663133cdaa61e336cfeaf2026d4b836
Bharath C V <bharathcv26@gmail.com>
2025-12-28 23:55:31 +0530

    updated

---
+++ main.tf
@@ -0,0 +1,27 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "my-unique-static-site-bucket"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-Website"
+  }
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-Website"
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction { restriction_type = "none" }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
